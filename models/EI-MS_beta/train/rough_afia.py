2229eb85718ab1b8c81deaf001549308a93d405c
Afia Anjum <56650056+Afia-Anjum@users.noreply.github.com>
2024-11-04 16:13:18 -0700

    Update rough_afia.py

--- models/EI-MS_beta/train/rough_afia.py
+++ models/EI-MS_beta/train/rough_afia.py
@@ -12,18 +12,10 @@ from utils import data_utils, train_utils, write_utils
 import pdb
 
 parameters={"loss":['ls', 'lad', 'huber'],
-    #"loss":['ls', 'lad', 'huber', 'quantile'],
-    #"learning_rate": [0.01, 0.025, 0.05, 0.075, 0.1, 0.15, 0.2],
     "learning_rate": [0.05,0.1, 0.15, 0.2],
-    #"min_samples_split": [2,4,6], #doesn't matter
-    #"min_samples_leaf": [1,2,4,6,8], #doesn't matter
-    #"max_depth":[3,5,8,10,12,15],
     "max_depth":[3,5,8,10],
-    #"max_features":["log2","sqrt"],
     "max_features":["sqrt"],
-    #"criterion": ["mae"],
     "subsample":[0.5,0.7,0.8, 0.85],
-    #"subsample":[0.5, 0.618, 0.8, 0.85, 0.9, 0.95, 1.0],
     "n_estimators":[10,100,200,300,400]
     }
 	
